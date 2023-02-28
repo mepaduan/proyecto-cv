8de98d837d6cc61df26e190c35975b1b4433f5c1
Marcelo Paduan <marcelopaduan@gmail.com>
2023-02-28 12:15:48 -0300

    primera subida

---
+++ js/function.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Cambio de texto.";
+  }
+
+
+
+  function mensaje() {
+    alert("Gracias por visitar mi CV.")
+    
+  }
